80230fa018d23c330ed3843b7386c1b5e470c887
ArnavPathela <147943352+ArnavPathela@users.noreply.github.com>
2024-10-07 09:27:26 +0530

    Update classStudent.py

--- LAB6/classStudent.py
+++ LAB6/classStudent.py
@@ -3,8 +3,7 @@ class student:
         self.name = name
         self.rollno = rollno
 
-student1 = student("Bob",2432)
-student2 = student("Sam",2433)
+student1 = student("John",2)
 
 print(f"student 1 details: name - {student1.name} , rollno - {student1.rollno}")
-print(f"student 2 details: name - {student2.name} , rollno - {student2.rollno}")
+
