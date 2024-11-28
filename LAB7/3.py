05c4d7d154c5168dffc3f9f004e1cffcebe4fc8b
ArnavPathela <147943352+ArnavPathela@users.noreply.github.com>
2024-11-28 18:05:54 +0530

    Create 3.py

---
+++ LAB7/3.py
@@ -0,0 +1,8 @@
+def selection_sort(data):
+    for i in range(len(data)):
+        min_idx = i
+        for j in range(i + 1, len(data)):
+            if data[j] < data[min_idx]:
+                min_idx = j
+        data[i], data[min_idx] = data[min_idx], data[i]
+    return data
