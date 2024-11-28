72dad01f8cacd46e4b012b6d568bd55579dc3bae
ArnavPathela <147943352+ArnavPathela@users.noreply.github.com>
2024-11-28 18:07:39 +0530

    Create 7.py

---
+++ LAB7/7.py
@@ -0,0 +1,34 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        if self.root is None:
+            self.root = Node(value)
+        else:
+            self._insert(self.root, value)
+
+    def _insert(self, current_node, value):
+        if value < current_node.value:
+            if current_node.left is None:
+                current_node.left = Node(value)
+            else:
+                self._insert(current_node.left, value)
+        elif value > current_node.value:
+            if current_node.right is None:
+                current_node.right = Node(value)
+            else:
+                self._insert(current_node.right, value)
+
+    def in_order_traversal(self, node, values=[]):
+        if node is not None:
+            self.in_order_traversal(node.left, values)
+            values.append(node.value)
+            self.in_order_traversal(node.right, values)
+        return values
